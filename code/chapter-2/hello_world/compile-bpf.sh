79192508df0f2cfb9a783550fcd176104e65ad75
Yifei Ma <yma@adaptiveinsights.com>
2019-12-18 18:06:33 -0800

    added a simple Makefile

--- code/chapter-2/hello_world/compile-bpf.sh
+++ code/chapter-2/hello_world/compile-bpf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export C_INCLUDE_PATH=/kernel-src/tools/testing/selftests/bpf
 clang -O2 -target bpf \
   -c bpf_program.c \
   -o hello_world_kern.o
--- code/chapter-2/hello_world/compile-loader.sh
+++ code/chapter-2/hello_world/compile-loader.sh
@@ -4,6 +4,7 @@ TOOLS=/kernel-src/samples/bpf
 INCLUDE=/kernel-src/tools/lib
 PERF_INCLUDE=/kernel-src/tools/perf
 KERNEL_TOOLS_INCLUDE=/kernel-src/tools/include/
+LIBRARY_PATH=/urs/local/lib64/
 BPFSO=bpf
 
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib64
