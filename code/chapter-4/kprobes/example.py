0c73fa0b1d3dfcc05410a723e6f5c9491a8c0c37
Lorenzo Fontana <lo@linux.com>
2019-12-25 21:52:07 +0100

    chapter-4: remove unneded void references
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- code/chapter-4/kprobes/example.py
+++ code/chapter-4/kprobes/example.py
@@ -3,7 +3,7 @@ from bcc import BPF
 bpf_source = """
 #include <uapi/linux/ptrace.h>
 
-int do_sys_execve(struct pt_regs *ctx, void filename, void argv, void envp) {
+int do_sys_execve(struct pt_regs *ctx) {
   char comm[16];
   bpf_get_current_comm(&comm, sizeof(comm));
   bpf_trace_printk("executing program: %s", comm);
@@ -11,7 +11,7 @@ int do_sys_execve(struct pt_regs *ctx, void filename, void argv, void envp) {
 }
 """
 
-bpf = BPF(text = bpf_source)
+bpf = BPF(text=bpf_source)
 execve_function = bpf.get_syscall_fnname("execve")
-bpf.attach_kprobe(event = execve_function, fn_name = "do_sys_execve")
+bpf.attach_kprobe(event=execve_function, fn_name="do_sys_execve")
 bpf.trace_print()
