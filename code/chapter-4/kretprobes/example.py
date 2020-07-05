0b769a76a985dd8ef16ea2eff0625bd409aef751
baiwfg2 <baiwfg2@gmail.com>
2020-07-05 11:04:21 +0800

    Add and fix some code

--- code/chapter-4/kretprobes/example.py
+++ code/chapter-4/kretprobes/example.py
@@ -9,7 +9,7 @@ int ret_sys_execve(struct pt_regs *ctx) {
   bpf_get_current_comm(&comm, sizeof(comm));
   return_value = PT_REGS_RC(ctx);
 
-  bpf_trace_printk("program: %s, return: %d", comm, return_value);
+  bpf_trace_printk("program: %s, return: %d\\n", comm, return_value);
   return 0;
 }
 """
