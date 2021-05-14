ffbed97c109bcceeef0710079c766f3be02a9f9a
liucimin <lcmjkson@gmail.com>
2021-05-14 23:30:12 +0800

    add chapter-6 tc add map counters

--- code/chapter-6/tc-flow-bpf-cls/lcm_classifier_kern.c
+++ code/chapter-6/tc-flow-bpf-cls/lcm_classifier_kern.c
@@ -1,76 +1,88 @@
-#pragma clang diagnostic ignored "-Wcompare-distinct-pointer-types"
+/* Copyright (c) 2016 Facebook
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ */
+#define KBUILD_MODNAME "foo"
+#include <uapi/linux/bpf.h>
+#include <uapi/linux/if_ether.h>
+#include <uapi/linux/if_packet.h>
+#include <uapi/linux/ip.h>
+#include <uapi/linux/ipv6.h>
+#include <uapi/linux/in.h>
+#include <uapi/linux/tcp.h>
+#include <uapi/linux/filter.h>
+#include <uapi/linux/pkt_cls.h>
+#include <net/ipv6.h>
+#include "bpf_helpers.h"
 
-#include <bits/types.h>
-#include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/in.h>
-#include <linux/ip.h>
-#include <linux/pkt_cls.h>
-#include <linux/tcp.h>
+#define _htonl __builtin_bswap32
 
-#define SEC(NAME) __attribute__((section(NAME), used))
-
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define __bpf_htons(x) __builtin_bswap16(x)
-#define __bpf_constant_htons(x) ___constant_swab16(x)
-#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-#define __bpf_htons(x) (x)
-#define __bpf_constant_htons(x) (x)
-#else
-#error "Fix your compiler's __BYTE_ORDER__?!"
-#endif
-
-#define bpf_htons(x) \
-  (__builtin_constant_p(x) ? __bpf_constant_htons(x) : __bpf_htons(x))
-
-static int (*bpf_trace_printk)(const char *fmt, int fmt_size,
-                               ...) = (void *)BPF_FUNC_trace_printk;
-
-#define trace_printk(fmt, ...)                                                 \
-  do {                                                                         \
-    char _fmt[] = fmt;                                                         \
-    bpf_trace_printk(_fmt, sizeof(_fmt), ##__VA_ARGS__);                       \
-  } while (0)
-
-unsigned long long load_byte(void *skb,
-                             unsigned long long off) asm("llvm.bpf.load.byte");
-
-struct http_payload {
-  int method;
+#define PIN_GLOBAL_NS		2
+struct bpf_elf_map {
+	__u32 type;
+	__u32 size_key;
+	__u32 size_value;
+	__u32 max_elem;
+	__u32 flags;
+	__u32 id;
+	__u32 pinning;
 };
 
+/* copy of 'struct ethhdr' without __packed */
+struct eth_hdr {
+	unsigned char   h_dest[ETH_ALEN];
+	unsigned char   h_source[ETH_ALEN];
+	unsigned short  h_proto;
+};
+
+struct bpf_elf_map SEC("maps") counter_map = {
+	.type = BPF_MAP_TYPE_ARRAY,
+	.size_key = sizeof(int),
+	.size_value = sizeof(int),
+	.pinning = PIN_GLOBAL_NS,
+	.max_elem = 256,
+};
+
+
 static inline int is_http(struct __sk_buff *skb, __u64 nh_off);
-
-typedef __uint8_t uint8_t;
-typedef __uint16_t uint16_t;
-typedef __uint32_t uint32_t;
-typedef __uint64_t uint64_t;
-
 SEC("classifier")
-static inline int classification(struct __sk_buff *skb) {
-  void *data_end = (void *)(long)skb->data_end;
-  void *data = (void *)(long)skb->data;
-  struct ethhdr *eth = data;
-
-  __u16 h_proto;
-  __u64 nh_off = 0;
+int classification(struct __sk_buff *skb)
+{
+	void *data = (void *)(long)skb->data;
+	struct eth_hdr *eth = data;
+	void *data_end = (void *)(long)skb->data_end;
+	int key = 0, *el;
+        int one = 1;
+     __u64 nh_off = 0;
   nh_off = sizeof(*eth);
 
-  if (data + nh_off > data_end) {
-    return TC_ACT_OK;
-  }
+	if (data + sizeof(*eth) > data_end)
+		return TC_ACT_OK;
+        key =   eth->h_proto;
+	el = bpf_map_lookup_elem(&counter_map, &key);
+if(el){
+ (*el)++;
+}
+else{
+  el = &one;
+}
+      bpf_map_update_elem(&counter_map, &key, el, BPF_ANY);
+  char msg[] = "Hello, BPF World!";
+  bpf_trace_printk(msg, sizeof(msg));
 
-  h_proto = eth->h_proto;
-  
-  trace_printk("Test!\n");
-  if (h_proto == bpf_htons(ETH_P_IP)) {
+  if (key == htons(ETH_P_IP)) {
     if (is_http(skb, nh_off) == 1) {
-      trace_printk("Yes! It is HTTP!\n");
+        char msg1[] = "HTTP!!!!!";
+        bpf_trace_printk(msg, sizeof(msg1));
     }
   }
 
-  return TC_ACT_OK;
+
+	return TC_ACT_OK;
 }
+
 
 static inline int is_http(struct __sk_buff *skb, __u64 nh_off) {
   void *data_end = (void *)(long)skb->data_end;
